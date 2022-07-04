d56df19959b403797228e719ceb252f432ba5c81
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-07-04 22:40:19 +0300

    Решение задачи 54

--- Task-54/Program.cs
+++ Task-54/Program.cs
@@ -12,3 +12,55 @@ using System;
 using static System.Console;
 using System.Linq;
 Clear();
+
+
+int m = 3; //Ряды массива
+int n = 4; //Столбцы массива
+
+int[,] arrayFirst = new int[m, n];
+
+FillArray(arrayFirst);
+PrintArray(arrayFirst);
+SortDescendingArray(arrayFirst);
+WriteLine();
+PrintArray(arrayFirst);
+
+void SortDescendingArray(int[,] arrayDescending)
+{
+   for (int i = 0; i < arrayDescending.GetLength(0); i++)
+   {
+      for (int j = 0; j < arrayDescending.GetLength(1); j++)
+      {
+         for (var k = 0; k < arrayDescending.GetLength(1); k++)
+         {
+            if (arrayDescending[i, j] < arrayDescending[i, k]) continue;
+            int temp = arrayDescending[i, j];
+            arrayDescending[i, j] = arrayDescending[i, k];
+            arrayDescending[i, k] = temp;
+         }
+      }
+   }
+}
+
+void FillArray(int[,] arrayFill)
+{
+   for (int i = 0; i < arrayFill.GetLength(0); i++)
+   {
+      for (int j = 0; j < arrayFill.GetLength(1); j++)
+      {
+         arrayFill[i, j] = new Random().Next(0, 10);
+      }
+   }
+}
+
+void PrintArray(int[,] arrayPrint)
+{
+   for (int i = 0; i < arrayPrint.GetLength(0); i++)
+   {
+      for (int j = 0; j < arrayPrint.GetLength(1); j++)
+      {
+         Write($"{arrayPrint[i, j]} ");
+      }
+      WriteLine();
+   }
+}
