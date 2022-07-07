bf8a855eeb265e429a115bfc516811ace2d7acef
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-07-07 19:03:37 +0300

    Решение задачи 60

--- Task-60/Program.cs
+++ Task-60/Program.cs
@@ -8,3 +8,65 @@
 using System;
 using static System.Console;
 Clear();
+
+int x = 2;
+int y = 2;
+int z = 2;
+
+int[,,] arrayFirst = new int[x, y, z];
+
+FillArray(arrayFirst);
+PrintArray(arrayFirst);
+
+void FillArray(int[,,] arrayFill) // Создаем двумерный массив
+{
+   bool serchNumber = true;
+   for (int i = 0; i < arrayFill.GetLength(0); i++)
+   {
+      for (int j = 0; j < arrayFill.GetLength(1); j++)
+      {
+         for (int k = 0; k < arrayFill.GetLength(2); k++)
+         {
+            int temp = new Random().Next(10, 100);
+            serchNumber = SerchArray(arrayFill, temp);
+            if (!serchNumber)
+            {
+               k--;
+               continue;
+            }
+            else arrayFill[i, j, k] = temp;
+         }
+      }
+   }
+}
+
+bool SerchArray(int[,,] arraySerch, int number) // Создаем двумерный массив
+{
+   for (int i = 0; i < arraySerch.GetLength(0); i++)
+   {
+      for (int j = 0; j < arraySerch.GetLength(1); j++)
+      {
+         for (int k = 0; k < arraySerch.GetLength(2); k++)
+         {
+            if (arraySerch[i, j, k] == number) return false;
+         }
+      }
+   }
+   return true;
+}
+
+void PrintArray(int[,,] arrayPrint) // Выводим в консоль двумерный массив
+{
+   for (int i = 0; i < arrayPrint.GetLength(0); i++)
+   {
+      for (int j = 0; j < arrayPrint.GetLength(1); j++)
+      {
+         for (int k = 0; k < arrayPrint.GetLength(2); k++)
+         {
+            Write($"{arrayPrint[i, j, k]}({i},{j},{k}) ");
+         }
+         WriteLine();
+      }
+      WriteLine();
+   }
+}
